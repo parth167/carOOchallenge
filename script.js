240609d2972eaf9b18baf9c4c14b65b0b6a6ef0a
patel <parthpatel167@gmail.com>
2024-01-14 13:40:10 -0600

    part 3 done and work fine also revEngine method gives output

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ class Car extends Vehicle {
     this.numWheels = 4;
   }
 }
+
+//part 3
+
+//for Motercycle
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+  revEngine() {
+    return 'VROOM VROOM!!!';
+  }
+}
+// let myFirstMotorcycle = new Motorcycle('Honda', 'Nighthawk', 2000);
+
+// myFirstMotorcycle.toString();
+// // "The vehicle is a Honda Nighthawk from 2000."
+
+// myFirstMotorcycle.honk(); // "Beep."
+// myFirstMotorcycle.revEngine(); // "VROOM!!!"
+// myFirstMotorcycle.numWheels; //2
